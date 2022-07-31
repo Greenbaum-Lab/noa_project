467f1e4df0fe17473f7c2659fa2f79764b55fb6a
Shahar <shahar.mazie@mail.huji.ac.il>
2022-07-31 18:05:33 +0300

    get genotype errors

--- get_genotype_error.py
+++ get_genotype_error.py
@@ -1,6 +1,8 @@
 from pipeline_runner import compute_genotype_error
 from utils import args_parser
+import os
 
 if __name__ == '__main__':
     arguments = args_parser()
+    os.makedirs(arguments.output, exist_ok=True)
     compute_genotype_error(arguments.repeated, arguments.input, arguments.output)
