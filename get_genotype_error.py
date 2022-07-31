32da8301e91a84dfa3a86c42d43e3a096f95ec74
Shahar <shahar.mazie@mail.huji.ac.il>
2022-07-31 18:11:20 +0300

    get genotype errors

--- get_genotype_error.py
+++ get_genotype_error.py
@@ -6,3 +6,5 @@ if __name__ == '__main__':
     arguments = args_parser()
     os.makedirs(arguments.output, exist_ok=True)
     compute_genotype_error(arguments.repeated, arguments.input, arguments.output)
+
+    print("Done!")
